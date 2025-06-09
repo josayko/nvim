8894b7ee89b98f529602122bac3782d121876012
Jonny Saykosy <josayko@pm.me>
2025-06-10 01:22:03 +0200

    feat: add which-key

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+vim.keymap.set("n", ";", ":", { desc = "CMD enter command mode" })
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
