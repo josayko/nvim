f1ac0a4fc0db75ee8b08f322749bcc8eab2e0880
Jonny Saykosy <josayko@pm.me>
2025-08-22 17:32:05 +0200

    feat: fix ts_ls and vue plugins

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -8,7 +8,7 @@ return {
     "typescriptreact",
     "typescript.tsx",
   },
-  root_markers = { "tsconfig.json", "package.json", ".git" },
+  root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
   init_options = {
     plugins = {
       {
--- lsp/vue_ls.lua
+++ lsp/vue_ls.lua
@@ -16,8 +16,26 @@ return {
   },
   init_options = {
     vue = {
-      -- disable hybrid mode
       hybridMode = false,
     },
+    typescript = {
+      tsdk = vim.fn.system("npm list -g typescript --parseable --depth=0 2>/dev/null | head -1"):gsub("\n", "")
+        .. "/lib",
+    },
   },
+  on_new_config = function(new_config, new_root_dir)
+    local function get_typescript_server_path(root_dir)
+      local project_root = vim.fs.find({ "node_modules" }, { path = root_dir, upward = true })[1]
+      if project_root then
+        local project_ts = project_root .. "/typescript/lib"
+        if vim.fn.isdirectory(project_ts) == 1 then
+          return project_ts
+        end
+      end
+      return vim.fn.system("npm list -g typescript --parseable --depth=0 2>/dev/null | head -1"):gsub("\n", "")
+        .. "/lib"
+    end
+
+    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+  end,
 }
